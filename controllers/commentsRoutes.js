c157ecbf5d6d9a54ff63f3a7a722fbef8cb46418
Luis Moore <luismoore@Luiss-MacBook-Air.local>
2024-03-04 19:56:20 +0000

    controllers update

---
+++ controllers/commentsRoutes.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+
+//login handler
+ router.get('/comments', (req, res) => {
+  
+    if (req.session.logged_in) {
+           res.render('comments');
+   }
+});
+module.exports = router;
